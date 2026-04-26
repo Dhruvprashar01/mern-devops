2abb91e9d68b5313c44097c0cfc7f2a049d206d9
Dhruv Mittal <dhruv@example.com>
2026-04-26 19:38:35 +0530

    first commit

---
+++ frontend/GlobalContext.tsx
@@ -0,0 +1,126 @@
+import {
+  createContext,
+  FunctionComponent,
+  ReactNode,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import API from "./src/utils/Api";
+
+export interface Products {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  images: string[];
+  category: string;
+  subCategory: string;
+  sizes: string[];
+  bestSeller: boolean;
+  createdAt: string;
+}
+export interface CartItem extends Products {
+  size: string;
+  quantity: number;
+  createdAt: string;
+}
+
+interface ContextType {
+  isDropdownOpen: boolean;
+  setIsDropdownOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isSearchBarOpen: boolean;
+  setIsSearchBarOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  cartItems: CartItem[];
+  setCartItems: React.Dispatch<React.SetStateAction<CartItem[]>>;
+  products: Products[];
+  setProducts: React.Dispatch<React.SetStateAction<Products[]>>;
+  subTotal: number;
+  setSubTotal: React.Dispatch<React.SetStateAction<number>>;
+  isUserDetailOpen: boolean;
+  setIsUserDetailOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  isPasswordHidden: boolean;
+  setIsPasswordHidden: React.Dispatch<React.SetStateAction<boolean>>;
+  togglePassword: () => void;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+}
+const GlobalContext = createContext<ContextType | undefined>(undefined);
+
+export const GlobalProvider: FunctionComponent<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const savedCartItems = JSON.parse(localStorage.getItem("cartItems") || "[]");
+
+  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const [isSearchBarOpen, setIsSearchBarOpen] = useState(false);
+  const [cartItems, setCartItems] = useState<CartItem[]>(savedCartItems);
+  const [products, setProducts] = useState<Products[]>([]);
+  const [subTotal, setSubTotal] = useState(0);
+  const [isUserDetailOpen, setIsUserDetailOpen] = useState(false);
+  const [isPasswordHidden, setIsPasswordHidden] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchedProducts = async () => {
+      setLoading(true);
+      try {
+        const res = await API.get("/products");
+        setProducts(res.data);
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+    fetchedProducts()
+  }, []);
+
+  useEffect(() => {
+    setSubTotal(
+      cartItems.reduce((acc, item) => acc + item.price * item.quantity, 0)
+    );
+  }, [cartItems]);
+
+  useEffect(() => {
+    localStorage.setItem("cartItems", JSON.stringify(cartItems));
+  }, [cartItems]);
+
+  const togglePassword = () => {
+    setIsPasswordHidden(!isPasswordHidden);
+  };
+
+  return (
+    <GlobalContext.Provider
+      value={{
+        isDropdownOpen,
+        setIsDropdownOpen,
+        isSearchBarOpen,
+        setIsSearchBarOpen,
+        cartItems,
+        setCartItems,
+        products,
+        setProducts,
+        subTotal,
+        setSubTotal,
+        isUserDetailOpen,
+        setIsUserDetailOpen,
+        isPasswordHidden,
+        setIsPasswordHidden,
+        togglePassword,
+        loading,
+        setLoading,
+      }}
+    >
+      {children}
+    </GlobalContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  const context = useContext(GlobalContext);
+  if (!context) {
+    throw new Error("useGlobalContext must be used within a GlobalProvider");
+  }
+  return context;
+};
---
+++ frontend/src/Container.tsx
@@ -0,0 +1,18 @@
+import { type ReactNode } from "react";
+import { useGlobalContext } from "../GlobalContext";
+interface Container {
+  children: ReactNode;
+  className?: string;
+}
+
+ const Container = ({ children, className = "" }: Container) => {
+
+  const { isUserDetailOpen, setIsUserDetailOpen } = useGlobalContext()
+  return (
+    <div onClick={() => setIsUserDetailOpen(!isUserDetailOpen)} className={`px-4 sm:px-[5vw] md:px-[7vw] lg:px-[9vw] ${className}`}>
+      {children}
+    </div>
+  );
+};
+
+export default Container;
